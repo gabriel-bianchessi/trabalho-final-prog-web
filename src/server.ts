5332400e55374f74d490bcce7446e4a5a8934dbd
Gabriel <gabriel.bianchessi21@gmail.com>
2025-11-21 15:10:34 -0300

    feat: initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello, I'm under the water");
+import express, { type Request, type Response } from "express";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/api/health", (request: Request, response: Response) => {
+  return response.status(200).send({
+    message: "All ok",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server rodando em http://localhost:${PORT || 3000}`);
+});
